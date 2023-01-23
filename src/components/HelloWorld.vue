488b7be9307615f9fa8ccfb54da0b24c48f286ac
Vitaly <naturbeer@mail.ru>
2023-01-23 14:21:13 +0300

    Create To-Do App

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <h1>{{ title }}</h1>
+      <form @submit.prevent="AddTask">
+        <input type="text" />
+        <button type="submit">Add New Note</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, ref } from "vue";
+export default {
+  name: "HelloWorld",
+  setup() {
+    const title = ref("To-Do");
+    const state = reactive({
+      count: 0,
+      double: computed(() => console.log("Test")),
+    });
+    return { state, title };
+  },
+};
+</script>
+
+<style>
+.one {
+  border: 1px solid black;
+  width: 50px;
+  height: 50px;
+  margin: 0 auto;
+  background-color: rgb(230, 221, 221);
+  cursor: pointer;
+}
+
+button {
+  margin: 0 5px;
+}
+</style>
