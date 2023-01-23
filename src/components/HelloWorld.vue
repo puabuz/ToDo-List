f1728226363fb602f4fdf69326c2dbaae72c50d9
Vitaly <naturbeer@mail.ru>
2023-01-23 22:05:59 +0300

    Вывод и добавление tasks

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,30 +2,50 @@
   <div>
     <div class="wrapper">
       <h1>{{ title }}</h1>
-      <form @submit.prevent="AddTask">
-        <input type="text" />
+      <form @submit.prevent="addTask">
+        <input type="text" required="required" v-model="inputValue" />
+        <p>{{ inputValue }}</p>
         <button type="submit">Add New Note</button>
       </form>
+      <ul>
+        <li v-for="(task, idx) in tasks" :key="task.text">
+          {{ idx + 1 }}. {{ task.text }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-import { computed, reactive, ref } from "vue";
+import { reactive, ref } from "vue";
 export default {
   name: "HelloWorld",
   setup() {
     const title = ref("To-Do");
+    const inputValue = ref("");
+    const tasks = ref([]);
     const state = reactive({
-      count: 0,
-      double: computed(() => console.log("Test")),
+      // double: computed(() => console.log("Test")),
     });
-    return { state, title };
+    //Добавить таску
+    const addTask = () => {
+      tasks.value.push({
+        text: inputValue.value.trim(),
+        completed: false,
+        err: false,
+      });
+      inputValue.value = "";
+    };
+    return { state, title, inputValue, tasks, addTask };
   },
 };
 </script>
 
 <style>
+li {
+  list-style-type: none;
+}
+
 .one {
   border: 1px solid black;
   width: 50px;
